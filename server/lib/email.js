ceca5783a16a8619dfba44e9edec89e2f686b859
olit kenea <olit2950@gmail.com>
2026-03-08 10:08:45 +0300

    fix the forgot password logic

--- server/lib/email.js
+++ server/lib/email.js
@@ -2,7 +2,9 @@ import nodemailer from 'nodemailer';
 
 export const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: process.env.SMTP_HOST || 'smtp.gmail.com',
+        port: process.env.SMTP_PORT || 587,
+        secure: process.env.SMTP_PORT == 465, // true for 465, false for other ports
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
@@ -10,7 +12,7 @@ export const sendEmail = async (options) => {
     });
 
     const mailOptions = {
-        from: `"Bootcamp Support" <${process.env.EMAIL_USER}>`,
+        from: process.env.EMAIL_FROM || `"Bootcamp Support" <${process.env.EMAIL_USER}>`,
         to: options.email,
         subject: options.subject,
         text: options.message,
